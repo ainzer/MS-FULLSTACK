5e4b3812518ac57ae99e85339633fc0c8a451c39
Tony <127862337+ainzer@users.noreply.github.com>
2024-01-15 09:53:37 +0100

    15/01/2024

---
+++ CRUD/details.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Détails du Disque</title>
+    <!-- Ajouter la référence au fichier Bootstrap CSS (à télécharger ou à partir d'un CDN) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Ajoutez du CSS pour mettre en forme les éléments */
+        .disque {
+            display: flex;
+            align-items: center;
+        }
+        .disque img {
+            max-width: 25%; /* Pour réduire la taille de l'image */
+            height: auto;
+        }
+        .details {
+            flex-grow: 1;
+            padding: 0 20px; /* Espacement entre l'image et les détails */
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+
+</html>
